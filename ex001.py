b17a185ee02e5c455aa6311130701af7d1b3c4fe
gabrielpsarda12 <sarda.gabriel@gmail.com>
2024-02-06 19:12:06 -0300

    Create ex001.py

---
+++ ex001.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
+name = str(input('Digite seu nome: '))
+print('Bem-vindo, ' + name + '!!!')
+
+# Crie um programa que escreva "Olá, mundo" na tela.
+
+print('Olá, mundo!!!')
