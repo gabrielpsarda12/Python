4c0140f1c691c25206567bc957fdabb37793ab94
gabrielpsarda12 <sarda.gabriel@gmail.com>
2024-02-06 19:40:46 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles
+
+a = int(input('Numero 1: '))
+b = int(input('Numero 2: '))
+
+soma = a + b
+
+print('A soma entre {} + {} é: {}'.format(a, b, soma))
